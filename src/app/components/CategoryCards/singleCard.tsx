42f638b6603fc56ace9dae841a6d1d3a68a78625
Chamira Gunawardhana <chamiramadusankha96@gmail.com>
2024-09-26 10:37:07 +0530

    text size change in category card mobile

--- src/app/components/CategoryCards/singleCard.tsx
+++ src/app/components/CategoryCards/singleCard.tsx
@@ -23,7 +23,7 @@ const SingleCard = ({
           objectFit="cover"
           loading="lazy"
         />
-        <h2 className="font-bold text-blueGray uppercase lg:text-2xl text-sm">
+        <h2 className="font-bold text-blueGray uppercase lg:text-2xl text-xs">
           {title}
         </h2>
       </div>
