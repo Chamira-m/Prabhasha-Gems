f0ac8bbb11a3e9ff737fe018cc2546fdc2b74f55
Chamira Gunawardhana <chamiramadusankha96@gmail.com>
2024-09-28 18:11:58 +0530

    add responsive

--- src/app/components/Common/SingleProductCard.tsx
+++ src/app/components/Common/SingleProductCard.tsx
@@ -10,9 +10,9 @@ const SingleProductCard = ({
   const { image, title, price } = singleProductcardType;
 
   return (
-    <div className="bg-white rounded-xl shadow-md w-40 h-52 md:w-36 md:h-48 lg:w-[200px] lg:h-[320px] xl:w-[288px] xl:h-[393px] flex flex-col items-center justify-center">
+    <div className="bg-white rounded-xl shadow-md xs:w-[150px] xs:h-52 sm:w-44 sm:h-56 md:w-36 md:h-48 lg:w-[200px] lg:h-[320px] xl:w-[250px] xl:h-[350px] 2xl:w-[288px] 2xl:h-[393px] flex flex-col items-center justify-center">
       <Image
-        className="object-cover w-36 h-36 md:w-56 md:min-h-56 lg:w-60 lg:h-60 xl:w-72 xl:h-72"
+        className="object-cover xs:w-36 xs:h-36 sm:w-36 sm:h-36 md:w-56 md:min-h-56 lg:w-60 lg:h-60 xl:w-72 xl:h-72 2xl:w-72 2xl:h-72"
         src={image}
         alt={title}
         width={100}
